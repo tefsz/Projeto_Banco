502c722438ab080245e1d62be18d079ece6cc946
tefsz <stefaniebarbosa234@gmail.com>
2021-09-09 09:43:21 -0300

    adicionando nova conta corrente

--- myBank/ContaCorrente.cs
+++ myBank/ContaCorrente.cs
@@ -1,6 +1,15 @@
 public class ContaCorrente{
-     public string titular;
-     public int agencia;
-     public int conta;
-     public double saldo;
+     
+        public string titular;
+        public int agencia;
+        public int conta;
+        public double saldo;
+
+        public ContaCorrente(string titular, int agencia, int conta, double saldo)
+    {
+        this.titular = titular;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = saldo;
+    }
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,22 +6,15 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaStefanie = new ContaCorrente();
+            ContaCorrente contaDaStefanie = new ContaCorrente("Stefanie", 1234, 789, 100.50);
 
-            contaDaStefanie.titular = "Stefanie";
-            contaDaStefanie.agencia = 1234;
-            contaDaStefanie.conta = 789;
-            contaDaStefanie.saldo = 100.50;
+            ContaCorrente contaDoJoao = new ContaCorrente("Joao", 1234, 790, 120);
 
-            ContaCorrente contaDoJoao = new ContaCorrente();
-
-            contaDoJoao.titular = "Joao";
-            contaDoJoao.agencia = 1234;
-            contaDoJoao.conta = 790;
-            contaDoJoao.saldo = 120;
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 791, 150);
 
             Console.WriteLine("O titular da conta é:" + contaDaStefanie.titular);
             Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
+            Console.WriteLine("O titular da conta é:" + contaDaMaria.titular);
         }
     }
 }
