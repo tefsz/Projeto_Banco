444ee0f2fd848e1395e8f2a024ef847f78435129
tefsz <stefaniebarbosa234@gmail.com>
2021-09-09 09:08:32 -0300

    enviando alterações necessárias

---
+++ myBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaStefanie = new ContaCorrente();
+
+            contaDaStefanie.titular = "Stefanie";
+            contaDaStefanie.agencia = 1234;
+            contaDaStefanie.conta = 789;
+            contaDaStefanie.saldo = 100.50;
+
+            ContaCorrente contaDoJoao = new ContaCorrente();
+
+            contaDoJoao.titular = "Joao";
+            contaDoJoao.agencia = 1234;
+            contaDoJoao.conta = 790;
+            contaDoJoao.saldo = 120;
+
+            Console.WriteLine("O titular da conta é:" + contaDaStefanie.titular);
+            Console.WriteLine("O titular da conta é:" + contaDoJoao.titular);
+        }
+    }
+}
